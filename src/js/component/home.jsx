d1a1917b373be9ac7d7e5d9104b120c41fbddbf3
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2023-06-30 22:32:22 +0000

    fix

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,10 +9,10 @@ const Home = () => {
 		<div className="container">
 			<h1>To Do</h1>
 			<ul>
-				<li> 
+				<li key="search"> 
 					<input type="text" placeholder="What to do today?" onChange={(e)=> setInputValue(e.target.value)}
 					value={inputValue} onKeyDown={(e)=>{
-						if(e.key ==="Enter"){
+						if(e.key ==="Enter" && inputValue != ""){
 							setTodos(todos.concat([inputValue]));
 							setInputValue("");
 							
@@ -20,8 +20,8 @@ const Home = () => {
 					}}/>
 				</li>
 				{todos.map((item, index) => (
-				<li>	
-					<div className="todo" key={index}>{item}<span className="hiddenX"onClick={()=> setTodos(todos.filter((t, currentIndex)=> index != currentIndex))}><strong>X</strong></span></div>
+				<li key={index}>	
+					<div className="todo">{item}<span className="hiddenX"onClick={()=> setTodos(todos.filter((t, currentIndex)=> index != currentIndex))}><strong>X</strong></span></div>
 				</li>
 				))}
 			</ul>
