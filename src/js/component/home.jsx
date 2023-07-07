38d0cec83e5f10856be8c76060cac7485550d311
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2023-07-07 19:57:31 +0000

    almost done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,33 +1,108 @@
-import React, {useState} from "react";
-
-
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-	const [inputValue, setInputValue] = useState("")
-	const [todos, setTodos] = useState([])
-	return (
-		<div className="container">
-			<h1>To Do</h1>
-			<ul>
-				<li key="search"> 
-					<input type="text" placeholder="What to do today?" onChange={(e)=> setInputValue(e.target.value)}
-					value={inputValue} onKeyDown={(e)=>{
-						if(e.key ==="Enter" && inputValue != ""){
-							setTodos(todos.concat([inputValue]));
-							setInputValue("");
-							
-						}
-					}}/>
-				</li>
-				{todos.map((item, index) => (
-				<li key={index}>	
-					<div className="todo">{item}<span className="hiddenX"onClick={()=> setTodos(todos.filter((t, currentIndex)=> index != currentIndex))}><strong>X</strong></span></div>
-				</li>
-				))}
-			</ul>
-			<div>{todos.length ?  `${todos.length} tasks to do` : 'Add tasks!!'}</div>
-	</div>
-	);	
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha");
+      const data = await response.json();
+      setTodos(data);
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+  };
+
+  const addTodo = async () => {
+    if (inputValue === "") return;
+
+    const newTodo = {
+      label: inputValue,
+      done: false,
+    };
+
+    try {
+      const updatedTodos = [...todos, newTodo];
+
+      const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedTodos),
+      });
+
+      if (response.ok) {
+        setTodos(updatedTodos);
+        setInputValue("");
+      } else {
+        console.error("Error adding todo:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error adding todo:", error);
+    }
+  };
+
+  const deleteTodo = async (index) => {
+    const updatedTodos =  todos.filter((todo, currentIndex) => currentIndex !== index);
+  
+    try {
+      const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedTodos),
+      });
+  
+      if (response.ok) {
+        setTodos(updatedTodos);
+      } else {
+        console.error("Error deleting todo:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addTodo();
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>To Do</h1>
+      <ul>
+        <li key="search">
+          <input
+            type="text"
+            placeholder="What to do today?"
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+            value={inputValue}
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            <div className="todo">
+              {item.label}
+              <span className="hiddenX" onClick={() => deleteTodo(index)}>
+                <strong>X</strong>
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div>{todos.length ? `${todos.length} tasks to do` : "Add tasks!!"}</div>
+    </div>
+  );
 };
 
 export default Home;
