1a393ed76b584a49655aad710134a63de6a82e82
christopher garcia <71852007+christopher-garcia1@users.noreply.github.com>
2023-07-08 00:02:18 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from "react";
 
+const TodoItem = ({ item, onDelete }) => {
+  return (
+    <li>
+      <div className="todo">
+        {item}
+        <span className="hiddenX" onClick={onDelete}>
+          <strong>X</strong>
+        </span>
+      </div>
+    </li>
+  );
+};
+
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
@@ -10,7 +23,9 @@ const Home = () => {
 
   const fetchTodos = async () => {
     try {
-      const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha");
+      const response = await fetch(
+        "https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha"
+      );
       const data = await response.json();
       setTodos(data);
     } catch (error) {
@@ -26,40 +41,66 @@ const Home = () => {
       done: false,
     };
 
+    const updatedTodos = [...todos, newTodo];
+
     try {
-      const updatedTodos = [...todos, newTodo];
+      await fetch(
+        "https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedTodos),
+        }
+      );
 
-      const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(updatedTodos),
-      });
-
-      if (response.ok) {
-        setTodos(updatedTodos);
-        setInputValue("");
-      } else {
-        console.error("Error adding todo:", response.statusText);
-      }
+      setTodos(updatedTodos);
+      setInputValue("");
     } catch (error) {
       console.error("Error adding todo:", error);
     }
   };
 
   const deleteTodo = async (index) => {
-    const updatedTodos =  todos.filter((todo, currentIndex) => currentIndex !== index);
-  
+    const updatedTodos = todos.filter((_, currentIndex) => currentIndex !== index);
+
     try {
-      const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(updatedTodos),
-      });
-  
+      let response;
+      if (updatedTodos.length) {
+        response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedTodos),
+        });
+      } else {
+        response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Error deleting todo");
+        }
+
+        // Create the user "jimmyhaha" with an empty todo list using POST
+        response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([]),
+        });
+
+        if (!response.ok) {
+          throw new Error("Error creating user");
+        }
+      }
+
       if (response.ok) {
         setTodos(updatedTodos);
       } else {
@@ -70,9 +111,33 @@ const Home = () => {
     }
   };
 
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      addTodo();
+  const deleteAllTodos = async () => {
+    try {
+      await fetch(
+        "https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha",
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+  
+      // Create the user "jimmyhaha" with an empty todo list using POST
+      await fetch(
+        "https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([]),
+        }
+      );
+  
+      setTodos([]); // Set todos to an empty array
+    } catch (error) {
+      console.error("Error deleting all todos:", error);
     }
   };
 
@@ -84,23 +149,27 @@ const Home = () => {
           <input
             type="text"
             placeholder="What to do today?"
-            onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={handleKeyDown}
             value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                addTodo();
+              }
+            }}
           />
         </li>
-        {todos.map((item, index) => (
-          <li key={index}>
-            <div className="todo">
-              {item.label}
-              <span className="hiddenX" onClick={() => deleteTodo(index)}>
-                <strong>X</strong>
-              </span>
-            </div>
-          </li>
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            item={todo.label}
+            onDelete={() => deleteTodo(index)}
+          />
         ))}
       </ul>
       <div>{todos.length ? `${todos.length} tasks to do` : "Add tasks!!"}</div>
+      <button className="deleteAllButton" onClick={deleteAllTodos}>
+        Delete All
+      </button>
     </div>
   );
 };
